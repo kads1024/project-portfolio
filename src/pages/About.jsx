989794148b5c1304dbe7447489d54380f65f9cb3
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2024-12-27 16:00:35 +0800

    Added Intro Work Experience

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -35,6 +35,21 @@ const About = () => {
           ))}
         </div>
       </div>
+      <div className='py-16'>
+          <h3 className='subhead-text'>
+            Work Experience
+          </h3>
+          <div className='mt-5 flex flex-col gap-3 text-slate-500'>
+            <p>
+              I've worked with all sorts of companies, leveling up my 
+              skills and teaming up with smart people. Here's the 
+              rundown:
+            </p>
+          </div>
+          <div className='mt-12 flex'>
+
+          </div>
+      </div>
     </section>
   )
 }
