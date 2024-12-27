3019cf3a9a08b53668209ca95ea82df1c62e8eab
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2024-12-27 15:08:26 +0800

    Initial Fox Model

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,9 @@
-import React, {useState, useRef} from 'react'
+import React, {useState, useRef, Suspense} from 'react'
 import emailjs from '@emailjs/browser'
+import { Canvas } from '@react-three/fiber';
+
+import Fox from '../models/Fox';
+import Loader from '../components/Loader';
 
 const Contact = () => {
   const formRef = useRef(null);
@@ -99,6 +103,21 @@ const Contact = () => {
             {isLoading ? 'Sending...' : 'Send Message'}
           </button>
         </form>
+      </div>
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+        <Canvas
+        camera={{
+          position: [0, 0, 5]
+          }}
+        >
+          <Suspense fallback={<Loader />}>
+            <Fox 
+              position={[0.5, 0.35, 0]}
+              rotation={[12, 0, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
       </div>
     </section>
   )
