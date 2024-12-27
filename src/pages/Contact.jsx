01c117b7890ba94ca03bf887beaa16949258f826
Kenneth Amiel Santos <kennethamiel.santos@gmail.com>
2024-12-27 14:31:39 +0800

    Bind EmailJS Service to form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -15,7 +15,28 @@ const Contact = () => {
     setIsLoading(true);
 
     // TODO: Add your emailjs service ID and template ID
-    emailjs.sendForm('service_1x5z5z9', 'template_1x5z5z9', form, 'user_1x5z5z9');
+    emailjs.send(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      {
+          from_name: form.name,
+          to_name: "Kenneth",
+          from_email: form.email,
+          to_email: "dg.kennethsantos@gmail.com",
+          message: form.message,
+      },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(() => {
+      setIsLoading(false);
+      // TODO: Show Success Message
+      // TODO: Hide Alert
+
+      setform({name: '', email: '', message: ''});
+    }).catch((error) => {
+      setIsLoading(false);
+      console.log(error);
+      // TODO: Show Error Message
+    });
    };
    
    const handleFocus = () => {  };
